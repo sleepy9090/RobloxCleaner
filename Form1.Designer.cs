8f15daf8fe8abcc89403e7b2e998db27046e7bdf
Shawn M. Crawford <sleepy3d@gmail.com>
2015-11-28 12:01:42 -0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace RobloxCleaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(290, 206);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(113, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Clean";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(278, 142);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 230);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Roblox Cleaner 1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace RobloxCleaner {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            wipeRobloxFolder();
+            wipeRobloxRegistryEntries();
+
+            MessageBox.Show("Complete!", "Roblox Cleaner", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+        }
+
+        private void wipeRobloxFolder() {
+
+            this.textBox1.Clear();
+            // Delete if exists
+            // C:\Users\sleepy\AppData\Local\Roblox
+            // 
+            // C:\Users\<username>\AppData\Local\Microsoft\Windows\Temporary Internet Files\
+            // C:\Users\<username>\AppData\Local\Microsoft\Windows\Temporary Internet Files\Low\
+            string userPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            string robloxPath = userPath + @"\AppData\Local\Roblox";
+            //Console.WriteLine("User's path: " + userPath);
+            //Console.WriteLine("Roblox path: " + robloxPath);
+
+            this.textBox1.AppendText("Attempting to remove : " + robloxPath + "\n");
+
+            try {
+                Directory.Delete(robloxPath, true);
+
+                this.textBox1.AppendText("The Roblox directory was successfully removed." + "\n");
+            } catch (DirectoryNotFoundException ex) {
+                this.textBox1.AppendText("The Roblox directory was not found." + "\n");
+            } catch (Exception ex) {
+                this.textBox1.AppendText("An exception was thrown while trying to remove the Roblox directory: " + ex + "\n");
+            }
+
+            
+        }
+
+        private void wipeRobloxRegistryEntries() {
+            //Remove from registry:
+            //HKEY_CURRENT_USER\Software\ROBLOX Corporation
+            this.textBox1.AppendText(@"Attempting to remove registry key: HKEY_CURRENT_USER\Software\ROBLOX Corporation" + "\n");
+
+            try {
+                RegistryKey parentKey = Registry.CurrentUser;
+                RegistryKey softwareKey = parentKey.OpenSubKey("SOFTWARE", true);
+                softwareKey.DeleteSubKeyTree("ROBLOX Corporation");
+                softwareKey.Close();
+                parentKey.Close();
+                this.textBox1.AppendText("The Roblox registry key was successfully removed." + "\n");
+            } catch (ArgumentException ex) {
+                this.textBox1.AppendText("The Roblox registry entries were not found." + "\n");
+            } catch (Exception ex) {
+                this.textBox1.AppendText("An exception was thrown while trying to remove the Roblox registry key: " + ex + "\n");
+            }
+        }
+    }
+}
